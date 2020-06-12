e2733d56fe17c8b48daa8f3269171626d6930db1
Rafael Martins <rafael.martins92@outlook.com>
2020-06-12 16:09:32 -0300

    Setting Amazon SES

---
+++ backend/src/config/mail.ts
@@ -0,0 +1,21 @@
+interface IMailConfig {
+  driver: 'ethereal' | 'ses';
+
+  defaults: {
+    from: {
+      email: string;
+      name: string;
+    };
+  };
+}
+
+export default {
+  driver: process.env.MAIL_DRIVER || 'ethereal',
+
+  defaults: {
+    from: {
+      email: 'rafael.martins92@outlook.com',
+      name: 'Rafael Martins',
+    },
+  },
+} as IMailConfig;
